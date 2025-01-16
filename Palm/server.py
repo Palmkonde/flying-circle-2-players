65733b5d85ea047c7894c1e3bc216bac5759014a
PalmPalmPalmPalmPalm <93920339+Palmkonde@users.noreply.github.com>
2025-01-16 12:10:26 +0700

    Adding some codes

--- Palm/server.py
+++ Palm/server.py
@@ -2,5 +2,17 @@
 It should work independently
 
 Sending data
-
 """
+
+import socket
+
+HOST = "0.0.0.0"
+PORT = 5505
+
+class Server:  
+    player = {}
+    def __init__(self):
+        pass
+
+if __name__ == "__main__":
+    pass
